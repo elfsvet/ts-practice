2b58ba5d31e003813d8c5f22805770b082c654d8
elfsvet <stepanmatysik@gmail.com>
2022-11-15 12:59:06 -0500

    miniproject is up and running

--- miniproject/dist/index.js
+++ miniproject/dist/index.js
@@ -1,4 +1,48 @@
 "use strict";
-const btn = document.getElementById('btn');
-console.log(btn);
-console.log('hello'.replaceAll('l', '!'));
+// btn.addEventListener('click', function () {
+//     alert(input.value)
+//     input.value = ''
+// });
+// not null assertion  !!!!!!!! exclamation point
+// type assertions
+// let mystery: unknown = 'Hello World!!!';
+// const numChars = (mystery as string).length
+// type assertions manipulate dom
+// type assertions as as as as as
+const btn = document.getElementById('btn'); // or pre btn variable like: <HTMLButtonElement>btn
+// type assertions as as as as as
+const input = document.getElementById('todoinput');
+const form = document.querySelector('form');
+const list = document.getElementById('todolist');
+const todos = readTodos();
+// shorter version of using
+//! todos.forEach(element => createTodo(element))
+todos.forEach(createTodo);
+function readTodos() {
+    const todosJSON = localStorage.getItem('todos');
+    //   need to use type narrowing
+    if (todosJSON === null)
+        return [];
+    return JSON.parse(todosJSON);
+}
+// to work with events if it separate function and it doesn't know what the e is...
+function handleSubmit(e) {
+    e.preventDefault();
+    const newTodo = {
+        text: input.value,
+        completed: false,
+    };
+    createTodo(newTodo);
+    todos.push(newTodo);
+    localStorage.setItem('todos', JSON.stringify(todos));
+    input.value = '';
+}
+function createTodo(todo) {
+    const newLI = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    newLI.append(todo.text);
+    newLI.append(checkbox);
+    list.append(newLI);
+}
+form.addEventListener('submit', handleSubmit);
--- miniproject/src/index.ts
+++ miniproject/src/index.ts
@@ -1,4 +1,61 @@
-const btn = document.getElementById('btn');
-console.log(btn);
+// btn.addEventListener('click', function () {
+//     alert(input.value)
+//     input.value = ''
+// });
 
-console.log('hello'.replaceAll('l', '!'))
+// not null assertion  !!!!!!!! exclamation point
+
+// type assertions
+// let mystery: unknown = 'Hello World!!!';
+// const numChars = (mystery as string).length
+// type assertions manipulate dom
+// type assertions as as as as as
+const btn = document.getElementById('btn')! as HTMLButtonElement; // or pre btn variable like: <HTMLButtonElement>btn
+// type assertions as as as as as
+const input = document.getElementById('todoinput')! as HTMLInputElement;
+
+const form = document.querySelector('form')!;
+
+const list = document.getElementById('todolist')!;
+
+interface Todo {
+  text: string;
+  completed: boolean;
+}
+const todos: Todo[] = readTodos();
+// shorter version of using
+//! todos.forEach(element => createTodo(element))
+todos.forEach(createTodo);
+
+function readTodos(): Todo[] {
+  const todosJSON = localStorage.getItem('todos');
+  //   need to use type narrowing
+  if (todosJSON === null) return [];
+  return JSON.parse(todosJSON);
+}
+
+// to work with events if it separate function and it doesn't know what the e is...
+function handleSubmit(e: SubmitEvent) {
+  e.preventDefault();
+  const newTodo: Todo = {
+    text: input.value,
+    completed: false,
+  };
+  createTodo(newTodo);
+  todos.push(newTodo);
+  localStorage.setItem('todos', JSON.stringify(todos));
+
+  input.value = '';
+}
+
+function createTodo(todo: Todo) {
+  const newLI = document.createElement('li');
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+
+  newLI.append(todo.text);
+  newLI.append(checkbox);
+  list.append(newLI);
+}
+
+form.addEventListener('submit', handleSubmit);
