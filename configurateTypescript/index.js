2741710ff97502a44981a3d18b8d1ea3f1bf2e50
elfsvet <stepanmatysik@gmail.com>
2022-11-15 09:29:48 -0500

    started configuring TypeScript

---
+++ configurateTypescript/index.js
@@ -0,0 +1,11 @@
+"use strict";
+const norma = {
+    breed: 'Silkie',
+    eggsPerWeek: 4,
+    name: 'Norma',
+};
+const juniper = {
+    breed: 'Silkie',
+    eggsPerWeek: 5,
+    name: 'Juniper',
+};
