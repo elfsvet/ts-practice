0d85ddab3cd968402150d7d3c2f50ca2c49360dd
elfsvet <stepanmatysik@gmail.com>
2022-11-17 15:32:49 -0500

    added the modules

---
+++ modules/dist/index.js
@@ -0,0 +1,7 @@
+import { add, sample as randomSample, pi } from './utils.js';
+const sample = 123;
+randomSample([1112, 3213, 32323, 3344]);
+add(1, 3);
+console.log(randomSample([1112, 3213, 32323, 3344]));
+console.log(add(1, 3));
+console.log(pi);
---
+++ modules/src/index.ts
@@ -0,0 +1,10 @@
+import { add, sample as randomSample, pi } from './utils.js';
+import User, { userHelper } from './User.js';
+
+
+const sample = 123;
+randomSample([1112, 3213, 32323, 3344]);
+add(1, 3);
+console.log(randomSample([1112, 3213, 32323, 3344]));
+console.log(add(1, 3));
+console.log(pi);
